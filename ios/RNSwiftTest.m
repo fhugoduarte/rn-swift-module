39f5b80e2a6e4fa95af49299b7c40b7145c28bdc
fhugoduarte <h.duarte222@gmail.com>
2020-01-29 21:57:16 -0300

    feat: Create iOS native module with swift

---
+++ ios/RNSwiftTest.m
@@ -0,0 +1,16 @@
+//
+//  RNSwiftTest.m
+//  swiftTest
+//
+//  Created by HugoDuarte on 29/01/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "React/RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(RNSwift, NSObject)
+
+RCT_EXTERN_METHOD(increment)
+
+@end
